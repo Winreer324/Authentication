5c1f6a91ad1c2520c844823d67d4db03ccc43447
Winreer324 <32igor3@mail.ru>
2019-01-11 10:31:48 +0300

    Initial commit

---
+++ lib/main.dart
@@ -0,0 +1,38 @@
+import 'package:leadning_firebase_raja/fireBase/login_page.dart';
+import 'package:leadning_firebase_raja/fireBase/sign_up_page.dart';
+import 'package:leadning_firebase_raja/fireBase/home_page_aut.dart';
+
+import 'package:flutter/material.dart';
+
+void main() => runApp(Authentication());
+
+//lessons 23
+// Flutter - Authentication
+//link video https://www.youtube.com/watch?v=5v3n8bCrpv0&list=PLgGjX33Qsw-EMsLf8TmsYhKOCx2ALZiKi&index=32
+
+//lessons 24
+// Flutter - AuthenticationFlutter - Social Authentication with Google
+//link video https://www.youtube.com/watch?v=YDxGCwKNaOs&list=PLgGjX33Qsw-EMsLf8TmsYhKOCx2ALZiKi&index=33
+
+//lessons 25
+// Flutter - Social Authentication with Facebook
+//link video https://www.youtube.com/watch?v=XxZLHP0fGwk&list=PLgGjX33Qsw-EMsLf8TmsYhKOCx2ALZiKi&index=34
+
+class Authentication extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Authentication',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: LoginPage(),
+      routes: <String, WidgetBuilder>{
+        '/landingpage': (BuildContext context)=> new Authentication(),
+        '/signup': (BuildContext context) => new SignUpPage(),
+        '/homepage': (BuildContext context) => new HomePage()
+      },
+    );
+  }
+}
